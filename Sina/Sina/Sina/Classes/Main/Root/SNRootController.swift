3f91c299c7d66409375b86b43d272273849588bc
qianjianeng <329426491@qq.com>
2016-07-28 22:33:05 +0800

    build sina

---
+++ Sina/Sina/Sina/Classes/Main/Root/SNRootController.swift
@@ -0,0 +1,32 @@
+//
+//  SNRootController.swift
+//  Sina
+//
+//  Created by qianjn on 16/7/28.
+//  Copyright © 2016年 SF. All rights reserved.
+//
+
+import UIKit
+
+class SNRootController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
