df166e1bf30854644a1fee89cbd5be893f35d1e8
qianjianeng <329426491@qq.com>
2016-08-16 14:28:13 +0800

    安装cocoplds

---
+++ Sina/Sina/Classes/Main/Home/QRCode/TBQRCardController.swift
@@ -0,0 +1,130 @@
+//
+//  TBQRCardController.swift
+//  Sina
+//
+//  Created by qianjn on 16/8/14.
+//  Copyright © 2016年 SF. All rights reserved.
+//
+
+import UIKit
+
+class TBQRCardController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 1.设置标题
+        navigationItem.title = "我的名片"
+        
+        // 2.添加图片容器
+        view.addSubview(iconView)
+        
+        // 3.布局图片容器
+
+        iconView.backgroundColor = UIColor.red()
+        
+        // 4.生成二维码
+        let qrcodeImage = creatQRCodeImage()
+        
+        // 5.将生成好的二维码添加到图片容器上
+        iconView.image = qrcodeImage
+       
+    }
+
+    
+    private func creatQRCodeImage() -> UIImage{
+        // 1.创建滤镜
+        let filter = CIFilter(name: "CIQRCodeGenerator")
+        
+        // 2.还原滤镜的默认属性
+        filter?.setDefaults()
+        
+        // 3.设置需要生成二维码的数据
+        filter?.setValue("极客江南".data(using: String.Encoding.utf8), forKey: "inputMessage")
+        
+        // 4.从滤镜中取出生成好的图片
+        let ciImage = filter?.outputImage
+        
+        //        return UIImage(CIImage: ciImage!)
+        let bgImage = createNonInterpolatedUIImageFormCIImage(image: ciImage!, size: 300)
+        //let bgImage = createNon
+        // 5.创建一个头像
+        let icon = UIImage(named: "nange.jpg")
+        
+        // 6.合成图片(将二维码和头像进行合并)
+        let newImage = creteImage(bgImage: bgImage, iconImage: icon!)
+        
+        // 7.返回生成好的二维码
+        return newImage
+    }
+    
+    
+    /**
+     合成图片
+     
+     :param: bgImage   背景图片
+     :param: iconImage 头像
+     */
+    private func creteImage(bgImage: UIImage, iconImage: UIImage) -> UIImage
+    {
+        // 1.开启图片上下文
+        UIGraphicsBeginImageContext(bgImage.size)
+        // 2.绘制背景图片
+        bgImage.draw(in: CGRect(origin: CGPoint.zero, size: bgImage.size))
+        // 3.绘制头像
+        let width:CGFloat = 50
+        let height:CGFloat = width
+        let x = (bgImage.size.width - width) * 0.5
+        let y = (bgImage.size.height - height) * 0.5
+        iconImage.draw(in: CGRect(x: x, y: y, width: width, height: height))
+        // 4.取出绘制号的图片
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        // 5.关闭上下文
+        UIGraphicsEndImageContext()
+        // 6.返回合成号的图片
+        return newImage!
+    }
+    
+    /**
+     根据CIImage生成指定大小的高清UIImage
+     
+     :param: image 指定CIImage
+     :param: size    指定大小
+     :returns: 生成好的图片
+     */
+    private func createNonInterpolatedUIImageFormCIImage(image: CIImage, size: CGFloat) -> UIImage {
+        
+        let extent: CGRect = image.extent.integral
+        let scale: CGFloat = min(size/extent.width, size/extent.height)
+        
+        // 1.创建bitmap;
+        let width = extent.width * scale
+        let height = extent.height * scale
+        let cs: CGColorSpace = CGColorSpaceCreateDeviceGray()
+        let bitmapRef = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: cs, bitmapInfo: 0)!
+        
+        let context = CIContext(options: nil)
+        let bitmapImage: CGImage = context.createCGImage(image, from: extent)!
+        
+        bitmapRef.interpolationQuality = CGInterpolationQuality.none
+        bitmapRef.scale(x: scale, y: scale);
+        bitmapRef.draw(in: extent, image: bitmapImage);
+        
+        // 2.保存bitmap到图片
+        let scaledImage: CGImage = bitmapRef.makeImage()!
+        
+        return UIImage(cgImage: scaledImage)
+
+    }
+    
+    // MARK: - 懒加载
+    private lazy var iconView: UIImageView = {
+    
+        let img = UIImageView()
+        img.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
+        img.center = self.view.center
+        return img
+    
+    }()
+
+}
--- Sina/Sina/Classes/Main/Home/QRCode/TBQRCodeController.swift
+++ Sina/Sina/Classes/Main/Home/QRCode/TBQRCodeController.swift
@@ -32,6 +32,11 @@ class TBQRCodeController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func createMyCard(_ sender: AnyObject) {
+        
+        let vc = TBQRCardController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -64,6 +69,9 @@ class TBQRCodeController: UIViewController {
         print(output.availableMetadataObjectTypes)
         // 5.设置输出对象的代理, 只要解析成功就会通知代理
         output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        // 如果想实现只扫描一张图片, 那么系统自带的二维码扫描是不支持的
+        // 只能设置让二维码只有出现在某一块区域才去扫描
+        // output.rectOfInterest = CGRectMake(0.0, 0.0, 1, 1)
         
         // 添加预览图层
         view.layer.insertSublayer(previewLayer, at: 0)
@@ -112,6 +120,13 @@ class TBQRCodeController: UIViewController {
         return layer!
     }()
     
+    // 创建用于绘制边线的图层
+    private lazy var drawLayer: CALayer = {
+        let layer = CALayer()
+        layer.frame = UIScreen.main().bounds
+        return layer
+    }()
+    
     
 }
 
@@ -138,8 +153,98 @@ extension TBQRCodeController:UITabBarDelegate, AVCaptureMetadataOutputObjectsDel
     // 只要解析到数据就会调用
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, from connection: AVCaptureConnection!)
     {
+        // 0.清空图层
+        clearConers()
+
         // 注意: 要使用stringValue
         print(metadataObjects.last?.stringValue)
+        
+        
+        
+        // 2.获取扫描到的二维码的位置
+        //        print(metadataObjects.last)
+        // 2.1转换坐标
+        for object in metadataObjects
+        {
+            // 2.1.1判断当前获取到的数据, 是否是机器可识别的类型
+            if object is AVMetadataMachineReadableCodeObject
+            {
+                // 2.1.2将坐标转换界面可识别的坐标
+                let codeObject = previewLayer.transformedMetadataObject(for: object as! AVMetadataObject) as! AVMetadataMachineReadableCodeObject
+                //                print(codeObject)
+                // 2.1.3绘制图形
+                drawCorners(codeObject: codeObject)
+            }
+        }
+
+        
+    }
+
+    
+    
+    /**
+     绘制图形
+     
+     :param: codeObject 保存了坐标的对象
+     */
+    private func drawCorners(codeObject: AVMetadataMachineReadableCodeObject)
+    {
+        if codeObject.corners.isEmpty
+        {
+            return
+        }
+        
+        // 1.创建一个图层
+        let layer = CAShapeLayer()
+        layer.lineWidth = 4
+        layer.strokeColor = UIColor.red().cgColor
+        layer.fillColor = UIColor.clear().cgColor
+        
+        // 2.创建路径
+        //layer.path = UIBezierPath(rect: CGRect(x: 100, y: 100, width: 200, height: 200)).cgPath
+        let path = UIBezierPath()
+        var point = CGPoint.zero
+        var index: Int = 0
+        // 2.1移动到第一个点
+        // print(codeObject.corners.last)
+        // 从corners数组中取出第0个元素, 将这个字典中的x/y赋值给point
+        
+        (point).makeWithDictionaryRepresentation((codeObject.corners[index] as! CFDictionary))
+        index = index + 1
+        path.move(to: point)
+        
+        // 2.2移动到其它的点
+        while index < codeObject.corners.count
+        {
+            (point).makeWithDictionaryRepresentation((codeObject.corners[index] as! CFDictionary))
+            index = index + 1
+            path.addLine(to: point)
+                    }
+        // 2.3关闭路径
+        path.close()
+        
+        // 2.4绘制路径
+        layer.path = path.cgPath
+        
+        // 3.将绘制好的图层添加到drawLayer上
+        drawLayer.addSublayer(layer)
+    }
+
+    
+    /**
+     清空边线
+     */
+    private func clearConers(){
+        // 1.判断drawLayer上是否有其它图层
+        if drawLayer.sublayers == nil || drawLayer.sublayers?.count == 0{
+            return
+        }
+        
+        // 2.移除所有子图层
+        for subLayer in drawLayer.sublayers!
+        {
+            subLayer.removeFromSuperlayer()
+        }
     }
 
     
